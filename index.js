28ff4b6a430b1ecb5cebf35c4c0fb9703dc5e67d
rick <github email address>
2020-04-05 19:31:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-const testVar = {}
+const record = [
+	{ year: "2018", result: "L" },
+	{ year: "2017", result: "W" },
+	{ year: "2016", result: "N/A" },
+	//...
+];
+const testVar = {};
 
 function testFunc() {
-  return "hi"
+	return "hi";
+}
+
+function superbowlWin(array) {
+let result = array.find(obj => obj.result === 'W')
+if (result) {
+  return result.year
+}
 }
